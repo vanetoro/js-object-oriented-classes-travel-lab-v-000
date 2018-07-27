91f0ef0addf639e97fc2c9ef374d2e2425f5f227
Vanessa Toro <vanetoro84@gmail.com>
2018-07-27 19:38:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ class Driver{
     return (year - 1) - this.startDate.getFullYear()
   }
 }
+
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+  blocksTravelled(){
+    
+  }
+}
