e3173a0d3ca33d39e83079f55c54bda41d01baf8
Vanessa Toro <vanetoro84@gmail.com>
2018-07-27 19:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 class Driver{
   constructor(name, startDate){
     this.name = name
@@ -15,7 +17,7 @@ class Route{
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
 
-let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
   blocksTravelled(){
 
   }
