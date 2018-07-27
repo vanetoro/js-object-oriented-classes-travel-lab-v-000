17a7574a91a47514ca9f3a8ea45ad4c95b8a5425
Vanessa Toro <vanetoro84@gmail.com>
2018-07-27 19:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ class Route{
   constructor(beginningLocation, endingLocation){
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
-  }
+
 let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
   blocksTravelled(){
-    
+
   }
+}
 }
